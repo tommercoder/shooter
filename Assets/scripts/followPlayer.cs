a5563252d6810151a18c3e1573679aed456deb9b
tommercoder <48603935+tommercoder@users.noreply.github.com>
2021-04-02 20:55:22 +0300

    a

---
+++ Assets/scripts/followPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+{
+    public Transform player;
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, this.transform.position.z);
+    }
+}
---
+++ Assets/scripts/movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float speed = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;
+    
+    Vector2 moveVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveVector.x = Input.GetAxisRaw("Horizontal");
+        moveVector.y= Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", moveVector.x);
+        animator.SetFloat("Vertical", moveVector.y);
+        animator.SetFloat("speed", moveVector.sqrMagnitude);
+
+
+
+        
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVector * speed * Time.fixedDeltaTime);
+    }
+}
