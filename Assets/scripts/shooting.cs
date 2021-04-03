e06e4a5712c79c91804d9a3091cd47217a0008f9
tommercoder <48603935+tommercoder@users.noreply.github.com>
2021-04-03 19:06:41 +0300

    [TS] test shooting and three different animations

---
+++ Assets/scripts/shooting.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+
+    public Transform firePoint1;
+    public Animator anim;
+    public GameObject bullet = null;
+    public GameObject[] all_bullets;
+    public float force1 = 20f;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            shoot();
+            
+        }
+    }
+
+
+    void shoot()
+    {
+        GameObject bulletG = Instantiate(bullet, firePoint1.position, firePoint1.rotation);
+        if (tempController.instance.firstWave)
+        {
+            anim.SetTrigger("Shoot");
+            bullet = all_bullets[0];
+        }
+        else if(tempController.instance.secondWave)
+        {
+            anim.SetTrigger("shotgunShoot");
+            bullet = all_bullets[1];
+        }
+        else if(tempController.instance.thirdWave)
+        {
+            anim.SetTrigger("rifleShoot");
+        }
+
+        Rigidbody2D rb = bulletG.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint1.right*force1,ForceMode2D.Impulse);
+    }
+}
