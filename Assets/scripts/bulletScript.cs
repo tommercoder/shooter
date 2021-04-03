8edce3ffb9406052851f6771cea4e0ebde237635
Golem <segeda.olexandr@gmail.com>
2021-04-03 19:06:00 +0200

    123

--- Assets/scripts/bulletScript.cs
+++ Assets/scripts/bulletScript.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class bulletScript : MonoBehaviour
 {
     public GameObject gitEffect;
+    public int damage = 30;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         GameObject effect = Instantiate(gitEffect, transform.position, Quaternion.identity);
         Destroy(effect, 1f);
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if(enemy != null){
+            enemy.TakeDamage(damage);
+        }
+        Debug.Log(hitInfo.name);
         Destroy(gameObject);
     }
 }
