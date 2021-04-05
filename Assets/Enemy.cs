a4128bbe07e67f1521c88fe0bec1f109dd62d880
tommercoder <48603935+tommercoder@users.noreply.github.com>
2021-04-05 16:16:40 +0300

    fix

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -2,52 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
-
-	public int health = 100;
+public class Enemy : MonoBehaviour
+{
+	public static Enemy instance;
+    private void Awake()
+    {
+		instance = this;
+    }
+    public int health = 100;
 	public GameObject deathEffect;
 	public GameObject player;
 	public bool kicked = false;
-    private void Start()
-    {
+	public int damage;
+	public int scoreForDeath;
+	private void Start()
+	{
 		damage = 10;
 		scoreForDeath = 10;
 		player = GameObject.FindGameObjectWithTag("Player");
-    }
-    public void TakeDamage (int damage)
+	}
+	public void TakeDamage(int damage)
 	{
 		health -= damage;
 
 		if (health <= 0)
 		{
-			
+
 			Die();
 			//Destroy(gameObject);
 		}
 	}
 
-	void Die ()
+	void Die()
 	{
 		//Instantiate(deathEffect, transform.position, Quaternion.identity);
 		statistics.instance.scoreInt += scoreForDeath;
 		statistics.instance.score.text = "score:" + statistics.instance.scoreInt;
 		Destroy(gameObject);
 	}
-    private void FixedUpdate()
-    {
-        if (kicked)
-        {
-			player.GetComponent<Rigidbody2D>().AddForce(transform.right * 20, ForceMode2D.Impulse);
-			
+	private void FixedUpdate()
+	{
+		if (kicked)
+		{
+			///player.GetComponent<Rigidbody2D>().AddForce(transform.right * 20, ForceMode2D.Impulse);
+
 		}
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
+	}
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
 		if (collision.gameObject.CompareTag("Player"))
 		{
 			Debug.Log("enemy collide with player");
 			playerHealth.instance.currentHealth -= damage;
 			kicked = true;
-			
 
+
+		}
+	}
 }
