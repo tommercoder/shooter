3edfff3f5963a0cd05a78940c37839ee9164c6c5
Golem <segeda.olexandr@gmail.com>
2021-04-05 13:58:50 +0200

    Map and Time and Health

---
+++ Assets/UI/CountTimer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountTimer : MonoBehaviour
+{
+
+    float currentTime = 0;
+    float startingTime = 10;
+
+    [SerializeField] Text countDownText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countDownText.text = currentTime.ToString("0");
+        if(currentTime < 0){
+            currentTime = 0;
+        }
+    }
+}
