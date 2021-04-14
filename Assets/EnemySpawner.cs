c79bd7a41b17b4deeaf547a67adf8306c35db747
Golem <segeda.olexandr@gmail.com>
2021-04-14 16:50:24 +0200

    Spawn

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField]
+    private float spawnRadius = 1, time  =0.5f;
+
+    public GameObject[] enemies;
+    public GameObject player;
+
+    void Start()
+    {
+         StartCoroutine(SpawnAnEnemy());
+    }
+
+    IEnumerator SpawnAnEnemy(){
+        Vector2 spawnPos = player.transform.position;
+        spawnPos += Random.insideUnitCircle.normalized * spawnRadius;
+
+        Instantiate(enemies[Random.Range(0, enemies.Length)], spawnPos, Quaternion.identity);
+
+        yield return new WaitForSeconds(time);
+
+        StartCoroutine(SpawnAnEnemy());
+    }
+}
